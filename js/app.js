a8a27a58fae1c6a4452023338f0859e2c4ad5e6d
sdavdf <davidfueltala90@hotmail.com>
2023-06-08 19:43:08 -0500

    Taller 14

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 console.log("Componente vue");
 console.log(Vue);
 
-
-
+//Todo ese lemento pasa al control de Vue
+//Option API
 const app = Vue.createApp({
-    //opcion1,
-    //opcion2,
-    //opcion3,
+    
+    //template: `
+    //<h1>HOLA MUNDO</h1>
+    //<p>Desde Vue.JS</p>`
 
-    template: `
-    <h1>HOLA MUNDO</h1>
-    <p>Desde Vue.JS</p>
+    //Opcion del Optio                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  n API quye me permite declarar propiedades
+    //reactiva vinculando informacion de la vista con mi programacion js
+    data(){
+        return {
+            nombre:'Juan',
+            apellido:'Jumbo',
+            mensaje:'Hola mundo 2 desde Vue.JS'
+        }
+    },
 
-    `
+    //Opcion de metodos: permite declarar metodos js
+    methods:{
+        cambiarNombre(){
+            console.log('boton cambiar nombre')
+            //siempre this para utilizar la propiedad reactiva en js
+            this.nombre = 'David'
+        },
+        cambiarApellido(){
+            console.log('boton cambiar apellido')
+            this.apellido = 'Telo'
+        }
+    }
+
 })
 
 
---
+++ js/vFor.js
@@ -0,0 +1,8 @@
+
+const app = Vue.createApp({
+    
+    
+})
+
+
+app.mount('#myApp2')
